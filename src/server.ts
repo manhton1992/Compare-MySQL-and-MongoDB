39efabf77fc4d40aacf9432fb86f8445a193b440
Manh Ton Nguyen <manhton1992@gmail.com>
2021-06-25 23:04:26 +0200

    Rebuild project

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,16 @@
  * Landing file for NodeJs
  */
 
-/** Package imports  */
-import express from 'express';
-import mongoose from 'mongoose';
+import { globalErrorHandler, ignoreFavicon } from './middlewares/errorhandler.middleware';
+
 import bodyParser from 'body-parser';
 import config from 'config';
-
+/** Package imports  */
+import express from 'express';
 /** Module imports */
 import { globalRouter } from './controllers/router';
-import { globalErrorHandler, ignoreFavicon} from './middlewares/errorhandler.middleware';
+import mongoose from 'mongoose';
+import { servicesVersion } from 'typescript';
 
 /**Variables */
 var cors = require('cors');
@@ -24,6 +25,10 @@ var datetime = require('node-datetime');
 /** Setup Database */
 mongoose.connect(config.get('database.host'), {useNewUrlParser : true});
 
+/*
+mongoose.connect('mongodb://localhost:27017/aircraft', { useNewUrlParser: true }).
+  catch(error => handleError(error));
+  */
 /** */
 
 /** Setup Mysql DB */
@@ -34,7 +39,11 @@ app.use(ignoreFavicon);
 
  /** Start server */
 app.listen(config.get('server.port'), () => {
-  console.log('Aircraft Server is running...');
+  console.log('Aircraft Server is running ...');
 });
 
 console.log(process.env.NODE_EVN);
+
+function handleError(error: any): any {
+  throw new Error('Function not implemented.');
+}
